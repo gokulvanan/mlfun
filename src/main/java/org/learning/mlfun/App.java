868a6cf2d35ad30e01dd6f266235a567b89cdfdb
gokulvanan <gokulvanan@gmail.com>
2015-12-13 06:51:00 +0530

    first commit

---
+++ src/main/java/org/learning/mlfun/App.java
@@ -0,0 +1,13 @@
+package org.learning.mlfun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
